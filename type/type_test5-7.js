8f837529851d60093574801fb6abeeb1bd5c7c36
mariusfredriks1 <marius.strand.fredriksen@gmail.com>
2022-10-03 13:12:03 +0200

    bootstrap

---
+++ type/type_test5-7.js
@@ -0,0 +1,21 @@
+//Definerer tidsgrense
+let TIME_LIMIT = 60;
+ 
+// define quotes to be used
+let quotes_array = [
+  'Gresset er grønnere på andre siden av gjerdet',
+  'Å være sliten og nedfor og nedfor er ikke et tegn på svakhet, mest sannsynlig har du vært sterk for lenge',
+  'Jeg skulle ikke spise den, jeg skulle bare smake på den',
+  'Nøtter er ikke noe for en hel rev',
+  'Man skal ikke plage andre, man skal være grei og snill, og for øvrig kan man gjøre hva man vil',
+  'Her kommer klatremus lillemann, en mus som synge og spille kan',
+  'Det har jeg aldri gjort før, så det klarer jeg helt sikkert',
+  'Det var hyggelig at du kom, men enda hyggeligere at du går',
+  'Nordmenn tror på Gud, Allah, Altet og Ingenting. Nordmenn liker Grieg og Kygo, Hellbillies og Kari Bremnes',
+  'Det er typisk norsk å være god',
+  'Enten så går det fint, eller så går det over',
+  '',
+  '',
+  '',
+  '',
+];
