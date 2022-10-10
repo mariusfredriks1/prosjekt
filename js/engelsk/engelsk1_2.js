c9dfaf7e0873ca1d8ecb6214d6d68590e7ba00c7
mariusfredriks1 <marius.strand.fredriksen@gmail.com>
2022-10-10 11:58:08 +0200

    noe greier

--- js/engelsk/engelsk1_2.js
+++ js/engelsk/engelsk1_2.js
@@ -1,1 +1,32 @@
+const fargeArray = [
+  'blå',
+  'rød',
+  'grønn',
+  'gul',
+  'lilla',
+  'rosa',
+  'oransje',
+  'brun',
+  'hvit',
+  'sort',
+  'grå',
+];
+const hexArray = [
+  '#6495ED',
+  '#FF0000',
+  '#008000',
+  '#FFFF00',
+  '#800080',
+  '#FF00FF',
+  '#FF5733',
+  '#8B4513',
+  '#FFFFFF',
+  '#000000',
+  '##808080',
+];
+let tilfeldigFarge = hexArray[Math.floor(Math.random() * hexArray.length)];
 
+function velgTilfeldigFarge() {
+    fargeDiv.style.backgroundColor = tilfeldigFarge;
+}
+//indexOf?
